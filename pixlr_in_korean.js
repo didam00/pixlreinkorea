9c243ba66c962345547e116afff88a29c55cf90a
DIDAM00 <47744125+didam00@users.noreply.github.com>
2020-04-13 23:55:04 +0900

    Add files via upload

--- pixlr_in_korean.js
+++ pixlr_in_korean.js
@@ -5,13 +5,60 @@ function $$(a) {return document.querySelectorAll(a);}
 
 // 파일
 $$(".menu a")[0].innerText = "파일"
-$$(".menu a")[1].innerText = "파일"
-
+  $$(".menu a")[1].innerHTML = "새 이미지" + $$(".menu a span")[0].outerHTML;
+  $$(".menu a")[2].innerHTML = "이미지 불러오기" + $$(".menu a span")[1].outerHTML;
+  $$(".menu a")[3].innerHTML = "이미지 링크 불러오기"
+  $$(".menu a")[4].innerHTML = "STOCK에서 불러오기" + $$(".menu a span")[2].outerHTML;
+  $$(".menu a")[5].innerHTML = "요소 추가하기" + $$(".menu a span")[3].outerHTML;
+  $$(".menu a")[6].innerHTML = "저장하기" + $$(".menu a span")[4].outerHTML;
+  $$(".menu a")[7].innerHTML = "인쇄하기" + $$(".menu a span")[5].outerHTML;
+  $$(".menu a")[8].innerHTML = "프로젝트 닫기" + $$(".menu a span")[6].outerHTML;
+  
 // 편집
-
+$$(".menu a")[9].innerText = "편집"
+  $$(".menu a")[10].innerHTML = "되돌리기" + $$(".menu a span")[7].outerHTML;
+  $$(".menu a")[11].innerHTML = "재실행" + $$(".menu a span")[8].outerHTML;
+  $$(".menu a")[12].innerHTML = "자르기" + $$(".menu a span")[9].outerHTML;
+  $$(".menu a")[13].innerHTML = "복사하기" + $$(".menu a span")[10].outerHTML;
+  $$(".menu a")[14].innerHTML = "지우기" + $$(".menu a span")[11].outerHTML;
+  $$(".menu a")[15].innerHTML = "붙여넣기" + $$(".menu a span")[12].outerHTML;
+  $$(".menu a")[16].innerHTML = "자유 변형" + $$(".menu a span")[13].outerHTML;
+  $$(".menu a")[17].innerHTML = "자유 왜곡" + $$(".menu a span")[14].outerHTML;
+  $$(".menu a")[18].innerHTML = "변형"
+    $$(".menu a")[19].innerHTML = "왼쪽으로 회전 / 시계 반대 방향"
+    $$(".menu a")[20].innerHTML = "오른쪽으로 회전 / 시계 방향"
+    $$(".menu a")[21].innerHTML = "좌우반전"
+    $$(".menu a")[22].innerHTML = "상하반전"
+  $$(".menu a")[23].innerHTML = "외곽선"
+  $$(".menu a")[24].innerHTML = "채우기"
+  
 // 이미지
-
+$$(".menu a")[25].innerText = "이미지";
+  $$(".menu a")[26].innerHTML = "이미지 크기";
+  $$(".menu a")[27].innerHTML = "캔버스 크기";
+  $$(".menu a")[28].innerHTML = "이미지 회전";
+    $$(".menu a")[29].innerHTML = "왼쪽으로 회전 / 시계 반대 방향";
+    $$(".menu a")[30].innerHTML = "오른쪽으로 회전 / 시계 방향";
+    $$(".menu a")[31].innerHTML = "좌우반전";
+    $$(".menu a")[32].innerHTML = "상하반전";
+  $$(".menu a")[33].innerHTML = "자르기";
+  $$(".menu a")[34].innerHTML = "다듬기";
+  
 // 레이어
+$$(".menu a")[35].innerHTML = "레이어";
+  $$(".menu a")[36].innerHTML = "새 레이어";
+  $$(".menu a")[37].innerHTML = "레이어 복사";
+  $$(".menu a")[38].innerHTML = "이미지 레이어 생성";
+  $$(".menu a")[39].innerHTML = "요소 레이어 생성";
+  $$(".menu a")[40].innerHTML = "이미지 링크 레이어 생성";
+  $$(".menu a")[41].innerHTML = "레이어 삭제" + $$(".menu a span")[15].outerHTML;
+  $$(".menu a")[42].innerHTML = "텍스트 / 요소 래스터화";
+  $$(".menu a")[43].innerHTML = "아래와 병합" + $$(".menu a span")[16].outerHTML;
+  $$(".menu a")[44].innerHTML = "보이는 요소와 병합" + $$(".menu a span")[17].outerHTML;
+  $$(".menu a")[45].innerHTML = "모든 레이어 병합" + $$(".menu a span")[18].outerHTML;
+  $$(".menu a")[46].innerHTML = "위로 가져오기" + $$(".menu a span")[19].outerHTML;
+  $$(".menu a")[47].innerHTML = "아래로 보내기" + $$(".menu a span")[20].outerHTML;
+  $$(".menu a")[48].innerHTML = "자동 마스크 AI";
 
 // 선택
 
