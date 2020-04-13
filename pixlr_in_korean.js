9194a9cbb566cdb2c7e970a6f47c0d10240b7447
DIDAM00 <47744125+didam00@users.noreply.github.com>
2020-04-13 23:54:08 +0900

    Add files via upload

---
+++ pixlr_in_korean.js
@@ -0,0 +1,24 @@
+// pixlr magic
+
+function $(a) {return document.querySelector(a);}
+function $$(a) {return document.querySelectorAll(a);}
+
+// 파일
+$$(".menu a")[0].innerText = "파일"
+$$(".menu a")[1].innerText = "파일"
+
+// 편집
+
+// 이미지
+
+// 레이어
+
+// 선택
+
+// 조정
+
+// 필터
+
+// 보기
+
+// 도움말
