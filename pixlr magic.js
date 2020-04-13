f115e501b03603e72b0ab522da99f04087cde086
DIDAM00 <47744125+didam00@users.noreply.github.com>
2020-04-13 22:53:55 +0900

    Add files via upload

---
+++ pixlr magic.js
@@ -0,0 +1,14 @@
+// pixlr magic
+
+function $(a) {return document.querySelector(a);}
+function $$(a) {return document.querySelectorAll(a);}
+
+$$(".menu a")[0].innerText = "파일"
+$$(".menu a")[1].innerText = "편집"
+$$(".menu a")[2].innerText = "이미지"
+$$(".menu a")[3].innerText = "레이어"
+$$(".menu a")[4].innerText = "선택"
+$$(".menu a")[5].innerText = "조정"
+$$(".menu a")[6].innerText = "필터"
+$$(".menu a")[7].innerText = "보기"
+$$(".menu a")[8].innerText = "도움말"
